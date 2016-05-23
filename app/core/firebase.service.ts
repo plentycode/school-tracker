cbbfd3256fa89913627c968240519a72a1caddde
Carlos Mercado <civan.cim@gmail.com>
2016-05-23 18:25:26 -0500

    updated

--- app/core/firebase.service.ts
+++ app/core/firebase.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, In } from '@angular/core';
+import './IFirebase'
 import 'firebase';
+import { Observable, Subject } from 'rxjs';
 
 declare var firebase: IFirebase;
 
 @Injectable()
 export class FirebaseService {
-private auth: any;
-private database: any;
-private storage: any;
+    private auth: any;
+    private database: any;
+    private storage: any;
+    public loggedUser: any;
+    public onAuthChanged: Subject<any>;
+
     constructor() {
 
         let config = {
@@ -16,21 +21,34 @@ private storage: any;
             databaseURL: "https://school-tracker.firebaseio.com",
             storageBucket: "school-tracker.appspot.com",
         };
-        
+
+        this.onAuthChanged = new Subject();
+
         firebase.initializeApp(config);
         this.auth = firebase.auth();
         this.database = firebase.database();
         this.storage = firebase.storage();
         this.auth.onAuthStateChanged((user: any) => this.onAuthStateChanged(user));
     }
-    
+
     private onAuthStateChanged(user: any) {
         console.log(user);
+        this.onAuthChanged.next(user);
     }
-    
+
     public signIn() {
         let provider = new firebase.auth.GoogleAuthProvider();
-        this.auth.signInWithPopup(provider);
+        return this.auth.signInWithPopup(provider).catch((err)=> {
+            alert(err)
+        });
+    }
+    
+    public signInUserPassword() {
+       return this.auth.signInWithEmailAndPassword('user@gmail.com', 'password');
+    }
+
+    public signOut() {
+        this.auth.signOut();
     }
 
 }
--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,15 +1,33 @@
 import {Page} from 'ionic-angular';
 import {FirebaseService} from './../../core/firebase.service';
+import { Observable } from 'rxjs';
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html',
 })
 export class Page1 {
+  loggedUser: any;
   constructor(private _firebaseService: FirebaseService) {
-    
+    this.loggedUser = null;
+
+    this._firebaseService.onAuthChanged.subscribe((user) => { this.onAuthChanged(user) })
   }
   public signIn() {
     this._firebaseService.signIn();
   }
+
+  public signOut() {
+    this._firebaseService.signOut();
+  }
+  
+  public openUrl () {
+    window.open('http://www.google.com', '_system');
+  }
+
+  private onAuthChanged(user: any) {
+    this.loggedUser = user;
+  }
+  
+  
 
 }
